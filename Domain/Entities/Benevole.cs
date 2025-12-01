83f91f49019110fbce7f9cc13f67b937436f0cc2
MmeCarrier <miquiztli_6eme_jour@msn.com>
2025-12-01 11:23:24 +0100

    ajouté entité bénévole au domain

---
+++ Domain/Entities/Benevole.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Benevole
+    {
+        public Benevole(int id, string prenom, string? nom, string tel, string? adresse, bool estResponsable, bool formeFerme, bool formeReptile, bool formeContrat)
+        {
+            Id = id;
+            Prenom = prenom;
+            Nom = nom;
+            Tel = tel;
+            Adresse = adresse;
+            EstResponsable = estResponsable;
+            FormeFerme = formeFerme;
+            FormeReptile = formeReptile;
+            FormeContrat = formeContrat;
+        }
+
+        public int Id { get; }
+        public string Prenom { get; set;}
+        public string? Nom { get; set; }
+        public string Tel { get; set; }
+        public string? Adresse { get; set; }
+        public bool EstResponsable { get; set; }
+        public bool FormeFerme { get; set; }
+        public bool FormeReptile { get; set; }
+        public bool FormeContrat { get; set; }
+
+    }
+}
