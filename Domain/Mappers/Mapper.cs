23ab2ebfc3154471fbdc76c1ee339199e038e887
MmeCarrier <miquiztli_6eme_jour@msn.com>
2025-12-01 11:53:11 +0100

    Mapper ajouté

---
+++ Domain/Mappers/Mapper.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Mappers
+{
+    internal static class Mapper
+    {
+        internal static Benevole ToBenevole(this IDataRecord record)
+            {
+             return new Benevole((int)record["Id"], 
+                                 (string)record["Prenom"], 
+                                 (string)record["Nom"], 
+                                 (string)record["Tel"], 
+                                 (string)record["Adresse"], 
+                                 (bool)record["EstResponsable"], 
+                                 (bool)record["FormeFerme"], 
+                                 (bool)record["FormeReptile"], 
+                                 (bool)record["FormeContrat"]);
+            }
+    }
+}
