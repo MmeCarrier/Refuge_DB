0e3a0bee7807cea0de293fe6fed0adf10b316ee2
MmeCarrier <miquiztli_6eme_jour@msn.com>
2025-12-02 15:35:44 +0100

    Ajouté service (me suis pris la tête) ajouté controller

--- Domain/Services/RefugeService.cs
+++ Domain/Services/RefugeService.cs
@@ -3,6 +3,7 @@ using Domain.Entities;
 using Domain.Repositories;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,16 @@ namespace Domain.Services
 {
     public class RefugeService : IRefugeRepository
     {
+        private readonly DbConnection _dbConnection;
+
+        public RefugeService(DbConnection dbConnection)
+        { 
+            _dbConnection = dbConnection;
+            if (_dbConnection.State is not System.Data.ConnectionState.Open)
+            {
+                _dbConnection.Open();
+            }
+        }
         bool ICqsResult.IsSuccess => throw new NotImplementedException();
 
         bool IcqsResult<Benevole>.IsSuccess => throw new NotImplementedException();
---
+++ RefugeManager.Api/Controllers/RefugeController.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Commands;
+using Domain.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using RefugeManager.Api.Models.Dtos;
+using Tools.Cqs.ToolResults;
+
+namespace RefugeManager.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RefugeController : ControllerBase    
+    {
+        private readonly IRefugeRepository _refugeRepository;
+        public RefugeController(IRefugeRepository refugeRepository)
+        {
+            _refugeRepository = refugeRepository;
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] AjoutBenevoleDto benevoledto)
+        {
+            ICqsResult result = _refugeRepository.Execute(new AjoutBenevole(benevoledto.Prenom, benevoledto.Nom, benevoledto.Tel, benevoledto.Adresse, benevoledto.EstResponsable, benevoledto.FormeFerme, benevoledto.FormeReptile, benevoledto.FormeContrat));
+                if (result.IsFailure)
+            {
+                return BadRequest(result);
+            }
+            return NoContent();
+        }
+    }
+}
