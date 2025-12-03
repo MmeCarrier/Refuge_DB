8e4d09ce7c1798aee1cfe8721e52227b5bdf3c9d
MmeCarrier <miquiztli_6eme_jour@msn.com>
2025-12-03 11:17:57 +0100

    Début ajout SupprimerBenevole (ajout domain commands)

---
+++ Domain/Commands/SupprimerBenevole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tools.Cqs.ToolCommands;
+
+namespace Domain.Commands
+{
+    public class SupprimerBenevole : ICommandDefinition
+    {
+        public SupprimerBenevole(int id)
+        {
+            id = Id;
+        }
+        public int Id { get;}
+    }
+}
