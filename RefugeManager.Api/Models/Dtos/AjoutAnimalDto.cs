542bb656c36ef3b03331e3b55aa05304370b9a64
MmeCarrier <miquiztli_6eme_jour@msn.com>
2025-12-04 15:22:43 +0100

    Ajout DTO AjoutAnimal et UpdateAnimal

--- RefugeManager.Api/Models/Dtos/AjoutAnimalDto.cs
+++ RefugeManager.Api/Models/Dtos/AjoutAnimalDto.cs
@@ -1,6 +1,45 @@
-﻿namespace RefugeManager.Api.Models.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RefugeManager.Api.Models.Dtos
 {
     public class AjoutAnimalDto
     {
+        public AjoutAnimalDto(string nom, string espece, string age, bool sterilise, bool mF, string primoVaccine, string vaccineComplet, string provenance, string lieuProvenance, string localisation, string remarque)
+        {
+            Nom = nom;
+            Espece = espece;
+            Age = age;
+            Sterilise = sterilise;
+            MF = mF;
+            PrimoVaccine = primoVaccine;
+            VaccineComplet = vaccineComplet;
+            Provenance = provenance;
+            LieuProvenance = lieuProvenance;
+            Localisation = localisation;
+            Remarque = remarque;
+        }
+
+        [StringLength(20, MinimumLength = 4)]
+        public string Nom { get; }
+        [Required]
+        [StringLength(20, MinimumLength = 4)]
+        public string Espece { get; }
+        [Range(1, int.MaxValue)]
+        public string Age { get; }
+        [Required]
+        public bool Sterilise { get; }
+        public bool MF { get; }
+        public string PrimoVaccine { get; }
+        public string VaccineComplet { get; }
+        [Required]
+        [StringLength(20, MinimumLength = 4)]
+        public string Provenance { get; }
+
+        [StringLength(80, MinimumLength = 4)]
+        public string LieuProvenance { get; }
+        [Required]
+        public string Localisation {get;}
+        [StringLength(20, MinimumLength = 10)]
+        public string Remarque { get; }
     }
 }
--- RefugeManager.Api/Models/Dtos/UpdateAnimalDto.cs
+++ RefugeManager.Api/Models/Dtos/UpdateAnimalDto.cs
@@ -1,6 +1,47 @@
-﻿namespace RefugeManager.Api.Models.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RefugeManager.Api.Models.Dtos
 {
     public class UpdateAnimalDto
     {
+        public UpdateAnimalDto(string nom, string espece, int age, bool mF, bool primoVaccin, bool vaccinComplet, string provenance, string lieuProvenance, string localisation, string remarque)
+        {
+            Nom = nom;
+            Espece = espece;
+            Age = age;
+            MF = mF;
+            PrimoVaccin = primoVaccin;
+            VaccinComplet = vaccinComplet;
+            Provenance = provenance;
+            LieuProvenance = lieuProvenance;
+            Localisation = localisation;
+            Remarque = remarque;
+        }
+
+        [StringLength(20, MinimumLength = 4)]
+        public string Nom { get; }
+        [Required]
+        [StringLength(20, MinimumLength = 4)]
+        public string Espece { get; }
+
+        public int Age { get; }
+        public bool MF { get; }
+        [Required]
+        public bool PrimoVaccin { get; }
+        [Required]
+        public bool VaccinComplet { get; }
+        [Required]
+        [StringLength(20, MinimumLength = 4)]
+        public string Provenance { get; }
+
+        [StringLength(80, MinimumLength = 4)]
+        public string LieuProvenance { get; }
+
+        [StringLength(20, MinimumLength = 4)]
+        public string Localisation { get; }
+
+        [StringLength(20, MinimumLength = 10)]
+        public string Remarque { get; }
+
     }
 }
