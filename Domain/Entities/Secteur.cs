5681bbfc302f0fa508c8c9cd98f8641b7fcc6db7
MmeCarrier <miquiztli_6eme_jour@msn.com>
2025-12-05 12:03:20 +0100

    ajout entité secteur (pour utiliser entity)

---
+++ Domain/Entities/Secteur.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Secteur // obligatoire de créer cette entité pour utiliser entity. obligé d'utiliser entity parce qu'il y a des classes partout avec des fk partout.
+    {
+        public Secteur(int secteurId, string nomSecteur)
+        {
+            SecteurId = secteurId;
+            NomSecteur = nomSecteur;
+        }
+
+        public int SecteurId { get; }
+        public string NomSecteur { get; }
+    }
+}
